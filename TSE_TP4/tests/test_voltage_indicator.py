691777e3b787359ddacd68386ea803d22dc51a22
milton-arg <milton.eduardo.sosa@gmail.com>
2020-12-10 20:37:49 +0100

    Se comienza a separar el codigo del test

--- TSE_TP4/tests/test_voltage_indicator.py
+++ TSE_TP4/tests/test_voltage_indicator.py
@@ -1,6 +1,7 @@
 import pytest
 import time
-from ..voltage_indicator import *
+#from ..voltage_indicator import Pin, voltage_indicator
+#from conftest import  DEBUG_ENABLE_PIN, RGB_LED, LED_OFF, RED, GREEN, VALOR_CRITICO
 
 """
 The HW will make use of the onboard RGB LED on the dev board to indicate 
@@ -11,14 +12,59 @@ NOTE: Use this feature FOR DEBUGGING PURPOSES ONLY.
 HOW TO USE IT:
 By default, this feature is ENABLED. Short 'P10' to GND, to disable it. 
 """
+
+#def pytest_configure():
+#    pytest.DEBUG_ENABLE_PIN = False
+#    pytest.RGB_LED = 0x000000
+#    pytest.LED_OFF = 0x000000
+#    pytest.RED = 0xff0000
+#    pytest.GREEN = 0x00ff00
+
 DEBUG_ENABLE_PIN = False
 
-RGB_LED = None
+RGB_LED = 0x000000
 LED_OFF = 0x000000
 RED = 0xff0000
 GREEN = 0x00ff00
 
 VALOR_CRITICO = 1965
+
+###-----------------MOCKUPs----------------------###
+class Pin:
+    """
+    This class is just a mimic of Pycom's Machine.Pin behaviour.
+
+    https://docs.pycom.io/firmwareapi/pycom/machine/pin/
+    """
+    IN = 1
+    PULL_UP = 1
+    def __init__(self, *args, **kwargs):
+        pass
+    def __call__(self):
+        global DEBUG_ENABLE_PIN
+        return DEBUG_ENABLE_PIN
+
+class pycom:
+    def rgbled(valor):
+        global RGB_LED
+        RGB_LED = valor
+
+
+###--------------------FUNCTION TO BE TESTED-------------------###
+def voltage_indicator(adc_value):
+    global RED
+    global GREEN
+    global VALOR_CRITICO
+
+    ENABLE_LED_INDICATOR = Pin('P10', mode=Pin.IN, pull=Pin.PULL_UP)
+
+    if ENABLE_LED_INDICATOR():
+        if adc_value < VALOR_CRITICO:
+            pycom.rgbled(RED)
+        if adc_value >= VALOR_CRITICO:
+            pycom.rgbled(GREEN)
+    else:
+            pycom.rgbled(LED_OFF)
 
 
 ###--------------------TESTs-------------------###
